3fafe3dd15fa071faafc4ac7d6b36ccc68e076df
TruongQuangDiep <quangdieptruong2005@gmail.com>
2026-05-10 15:43:06 +0700

    setup file env ok

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -2,12 +2,12 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // dùng SSL
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT, 
+    secure: process.env.EMAIL_PORT == 465, 
     auth: {
-        user: 'tqd200040@gmail.com', // Email của bạn
-        pass: 'wvnb svfw oqqe nkcg', // 16 ký tự App Password bạn vừa lấy
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
     },
 });
 
